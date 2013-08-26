664c605117c190a1588b691cd040dc5bf0a4fd51
raveit65 <chat-to-me@raveit.de>
2013-08-26 16:53:55 +0200

    properties: update to latest standalone in gnome-bluetooth

--- properties/main.c
+++ properties/main.c
@@ -140,7 +140,7 @@ static GtkWidget *create_window(GtkWidget *notebook)
 	gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
 	settings = g_settings_new (SCHEMA_NAME);
 	g_settings_bind (settings, PREF_SHOW_ICON, G_OBJECT (button), "active",
-			 G_SETTINGS_BIND_DEFAULT | G_SETTINGS_BIND_NO_SENSITIVITY);
+			 G_SETTINGS_BIND_DEFAULT);
 
 	buttonbox = gtk_hbutton_box_new();
 	gtk_button_box_set_layout(GTK_BUTTON_BOX(buttonbox), GTK_BUTTONBOX_END);
@@ -219,8 +219,6 @@ int main(int argc, char *argv[])
 	GtkWidget *notebook;
 	GError *error = NULL;
 
-	g_setenv ("GSETTINGS_BACKEND", "gconf", FALSE);
-
 	bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR);
 	bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
 	textdomain(GETTEXT_PACKAGE);
@@ -274,3 +272,4 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
+
